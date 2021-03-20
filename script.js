1cf40fdb1560743ee4749b3123d39868ce452d18
Andreea Chelaru <andreeache@gmail.com>
2021-03-20 14:57:40 +0100

    fixed tag buttons

--- script.js
+++ script.js
@@ -35,10 +35,35 @@ generateUstensils();
 
 $("#ingredient-group").on("show.bs.dropdown", function () {
   $(".filter__group-ingredient").css("width", "685px");
+  $(".filter__group-ingredient-dd").css("width", "685px");
   $(".filter__input-ingredient").attr("placeholder", "Search an ingredient");
 });
 
 $("#ingredient-group").on("hide.bs.dropdown", function () {
-  $(".filter__group-ingredient").css("width", "200px");
+  $(".filter__group-ingredient").css("width", "170px");
   $(".filter__input-ingredient").attr("placeholder", "Ingredients");
 });
+
+$("#device-group").on("show.bs.dropdown", function () {
+    $(".filter__group-devices").css("width", "685px");
+    $(".filter__group-devices-dd").css("width", "685px");
+    $(".filter__input-device").attr("placeholder", "Search a device");
+  });
+  
+  $("#device-group").on("hide.bs.dropdown", function () {
+    $(".filter__group-devices").css("width", "170px");
+    $(".filter__input-device").attr("placeholder", "Devices");
+  });
+
+  
+  $("#ustensils-group").on("show.bs.dropdown", function () {
+    $(".filter__group-ustensils").css("width", "685px");
+    $(".filter__group-ustensils-dd").css("width", "685px");
+    $(".filter__input-ustensils").attr("placeholder", "Search an ustensil");
+  });
+  
+  $("#ustensils-group").on("hide.bs.dropdown", function () {
+    $(".filter__group-ustensils").css("width", "170px");
+    $(".filter__input-ustensils").attr("placeholder", "Ustensils");
+  });
+  
