32a080b0839644297b9f935833be932299f8a542
Andreea Chelaru <andreeache@gmail.com>
2021-03-20 19:31:49 +0100

    generated recipe cards

---
+++ js/recipeCard.js
@@ -0,0 +1,107 @@
+import { recipes } from "./recipes.js";
+
+export const generateRecipes = () => {
+  const mainSection = document.getElementById("main-section");
+  recipes.forEach((recipe) => {
+    //create recipe card
+
+    let mainCard = document.createElement("DIV");
+    mainCard.setAttribute("class", "col-md-4 main-card d-flex");
+    mainSection.appendChild(mainCard);
+
+    let recipeCard = document.createElement("DIV");
+    recipeCard.setAttribute("class", "card m-3 bg-light recipe-card bg-light");
+    mainCard.appendChild(recipeCard);
+
+    let recipeCardTop = document.createElement("DIV");
+    recipeCardTop.setAttribute("class", "card-img-top recipe-card__top");
+    recipeCardTop.setAttribute("id", "missing-photo");
+    recipeCard.appendChild(recipeCardTop);
+
+    let recipeCardBody = document.createElement("DIV");
+    recipeCardBody.setAttribute("class", "card-body recipe-card__body");
+    recipeCard.appendChild(recipeCardBody);
+
+    let recipeCardHeader = document.createElement("DIV");
+    recipeCardHeader.setAttribute("class", "row recipe-card__header");
+    recipeCardBody.appendChild(recipeCardHeader);
+
+    let recipeCardTitle = document.createElement("DIV");
+    recipeCardTitle.setAttribute("class", "col-8 recipe-card__title");
+    recipeCardTitle.innerText = recipe["name"];
+    recipeCardHeader.appendChild(recipeCardTitle);
+
+    let recipeCardDuration = document.createElement("DIV");
+    recipeCardDuration.setAttribute(
+      "class",
+      "col-4 d-flex justify-content-around recipe-card__duration"
+    );
+    recipeCardHeader.appendChild(recipeCardDuration);
+
+    let timerIcon = document.createElement("DIV");
+    timerIcon.setAttribute("class", "recipe-card__duration-icon");
+    timerIcon.innerHTML = "w";
+    recipeCardDuration.appendChild(timerIcon);
+
+    let recipeDuration = document.createElement("DIV");
+    recipeDuration.setAttribute("class", "recipe-card__duration-numerals");
+    recipeDuration.innerText = recipe["time"];
+    recipeCardDuration.appendChild(recipeDuration);
+
+    let timeNumerals = document.createElement("DIV");
+    timeNumerals.setAttribute("classs", "recipe-card__duration-min");
+    timeNumerals.innerText = "min";
+    recipeCardDuration.appendChild(timeNumerals);
+
+    let recipeInstructions = document.createElement("DIV");
+    recipeInstructions.setAttribute(
+      "class",
+      "row instructions overflow-hidden recipe-card__instruction"
+    );
+    recipeCardBody.appendChild(recipeInstructions);
+
+    let recipeCardDetails = document.createElement("DIV");
+    recipeCardDetails.setAttribute("class", "col-6 recipe-card__details");
+    recipeInstructions.appendChild(recipeCardDetails);
+
+    // forEach -> generate the numeber of igrendients, name, quantity and units
+
+    recipe["ingredients"].forEach((ingredient) => {
+      let ingredients = document.createElement("div");
+      ingredients.setAttribute("class", "recipe-card__ingredients d-flex");
+      ingredients.setAttribute("id", "ingredient");
+      recipeCardDetails.appendChild(ingredients);
+
+      let ingredientName = document.createElement("DIV");
+      ingredientName.setAttribute("classs", "recipe-card__ingredients-name");
+      ingredientName.setAttribute("id", "recipe-card__ingredients-name");
+      ingredientName.innerText = ingredient["ingredient"];
+      ingredients.appendChild(ingredientName);
+
+      let ingredientsQty = document.createElement("DIV");
+      ingredientsQty.setAttribute("class", "recipe-card__quantity d-flex");
+      ingredients.appendChild(ingredientsQty);
+
+      let quantity = document.createElement("DIV");
+      quantity.setAttribute("id", "recipe-card__quantity-value");
+      quantity.innerText = ingredient["quantity"];
+      ingredientsQty.appendChild(quantity);
+
+      if (ingredient["unit"]) {
+        let qtyUnits = document.createElement("DIV");
+        qtyUnits.setAttribute("id", "recipe-card__quantity-units");
+        qtyUnits.innerText = ingredient["unit"];
+        ingredientsQty.appendChild(qtyUnits);
+      }
+    });
+
+    let recipeSteps = document.createElement("DIV");
+    recipeSteps.setAttribute(
+      "class",
+      "col-6 recipe-card__steps overflow-hidden "
+    );
+    recipeSteps.setAttribute("id", "description");
+    recipeSteps.innerText = recipe["description"];
+    recipeInstructions.appendChild(recipeSteps);
+  });
+};
---
+++ js/script.js
@@ -0,0 +1,70 @@
+import { recipes } from "./recipes.js";
+import { tagFactory } from "./tagFactory.js";
+import { generateRecipes } from "./recipeCard.js";
+
+const ingredientFilter = document.getElementById("ingredient-filter");
+
+const generateIngredients = () => {
+  const ingredientsDropdown = document.getElementById("ingredients-dropdown");
+
+  let t = new tagFactory(
+    ingredientsDropdown,
+    recipes,
+    "ingredients",
+    "ingredient"
+  );
+  t.generate();
+};
+
+generateIngredients();
+
+const generateDevices = () => {
+  const devicesDropdown = document.getElementById("devices-dropdown");
+  let t = new tagFactory(devicesDropdown, recipes, "appliance", null);
+  t.generate();
+};
+
+generateDevices();
+
+const generateUstensils = () => {
+  const devicesDropdown = document.getElementById("ustensils-dropdown");
+  let t = new tagFactory(devicesDropdown, recipes, "ustensils", "");
+  t.generate();
+};
+
+generateUstensils();
+
+$("#ingredient-group").on("show.bs.dropdown", function () {
+  $(".filter__group-ingredient").css("width", "685px");
+  $(".filter__group-ingredient-dd").css("width", "685px");
+  $(".filter__input-ingredient").attr("placeholder", "Search an ingredient");
+});
+
+$("#ingredient-group").on("hide.bs.dropdown", function () {
+  $(".filter__group-ingredient").css("width", "170px");
+  $(".filter__input-ingredient").attr("placeholder", "Ingredients");
+});
+
+$("#device-group").on("show.bs.dropdown", function () {
+  $(".filter__group-devices").css("width", "685px");
+  $(".filter__group-devices-dd").css("width", "685px");
+  $(".filter__input-device").attr("placeholder", "Search a device");
+});
+
+$("#device-group").on("hide.bs.dropdown", function () {
+  $(".filter__group-devices").css("width", "170px");
+  $(".filter__input-device").attr("placeholder", "Devices");
+});
+
+$("#ustensils-group").on("show.bs.dropdown", function () {
+  $(".filter__group-ustensils").css("width", "685px");
+  $(".filter__group-ustensils-dd").css("width", "685px");
+  $(".filter__input-ustensils").attr("placeholder", "Search an ustensil");
+});
+
+$("#ustensils-group").on("hide.bs.dropdown", function () {
+  $(".filter__group-ustensils").css("width", "170px");
+  $(".filter__input-ustensils").attr("placeholder", "Ustensils");
+});
+
+generateRecipes();
